c0284d3101f801b596dffcc31e50dfe4c4883cce
Volodymyr-bart <61430064+Volodymyr-bart@users.noreply.github.com>
2023-01-28 16:30:06 +0200

    deploy

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import { Chat } from "./component/Chat";
+
+function App() {
+  return (
+    <>
+      <Chat />
+    </>
+  );
+}
+
+export default App;
